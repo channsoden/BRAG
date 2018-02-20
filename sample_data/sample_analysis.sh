8835fde1d4d92234b7c4038422fbfd1c9b33726a
Christopher Hann-Soden <channsoden@berkeley.edu>
2018-02-19 20:39:04 -0800

    switched to explicit python2 for binder
    should convert this to python3 to be Good

--- sample_data/sample_analysis.sh
+++ sample_data/sample_analysis.sh
@@ -17,10 +17,10 @@ ln -s ../hannsoden-bioinformatics/gff_tools.py
 ln -s ../hannsoden-bioinformatics/plots.py
 
 # Generate extra tracks of data
-python extra_tracks.py
+python2 extra_tracks.py
 
 # Run BRAG
-../BRAG -t Neurospora.nwk -r Nc -x Sm -o best_Neuro -g alignments/Neurospora.*.os.txt -q alignments/Neurospora.*.seqs -C Neurospora-crassa_v12_fixed_centromeres.txt -T Ncra_extra_tracks.tsv
+python2 ../BRAG -t Neurospora.nwk -r Nc -x Sm -o best_Neuro -g alignments/Neurospora.*.os.txt -q alignments/Neurospora.*.seqs -C Neurospora-crassa_v12_fixed_centromeres.txt -T Ncra_extra_tracks.tsv
 
 # Generate figures from BRAG output
-python paper_figures.py
+python2 paper_figures.py
