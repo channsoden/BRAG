fa2f8494026d9488b4ab871988e893eaf91cecb6
channsoden <channsoden@gmail.com>
2018-02-21 16:11:58 -0800

    upgrade to python 3

--- sample_data/sample_analysis.sh
+++ sample_data/sample_analysis.sh
@@ -5,10 +5,10 @@ tar -xzf genomes.tar.gz
 PYTHONPATH=$PYTHONPATH:../hannsoden-bioinformatics:..
 
 # Generate extra tracks of data
-python2 extra_tracks.py
+python extra_tracks.py
 
 # Run BRAG
-python2 ../BRAG -t Neurospora.nwk -r Nc -x Sm -o best_Neuro -g alignments/Neurospora.*.os.txt -q alignments/Neurospora.*.seqs -C Neurospora-crassa_v12_fixed_centromeres.txt -T Ncra_extra_tracks.tsv
+time ../BRAG -t Neurospora.nwk -r Nc -x Sm -o best_Neuro -g alignments/Neurospora.*.os.txt -q alignments/Neurospora.*.seqs -C Neurospora-crassa_v12_fixed_centromeres.txt -T Ncra_extra_tracks.tsv
 
 # Generate figures from BRAG output
-python2 paper_figures.py
+python paper_figures.py
